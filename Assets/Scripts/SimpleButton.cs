236b7a8c22db8d2134eb1c438bac95dff04fd613
Tologon Eshimkanov <teshimkanov@gmail.com>
2021-06-08 18:15:30 +0200

    Add SimpleButton class
    Add a parent class

---
+++ Assets/Scripts/SimpleButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleButton : MonoBehaviour
+{
+    public void Action()
+    {
+        Debug.Log("SimpleButton -> ACTION!");
+    }
+}
