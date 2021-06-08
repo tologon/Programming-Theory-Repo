c0520c0adf47b7246a462d2f78bee27f2da0d46b
Tologon Eshimkanov <teshimkanov@gmail.com>
2021-06-08 18:49:13 +0200

    Add SizeButton

---
+++ Assets/Scripts/SizeButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class SizeButton : SimpleButton
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    public override void Action()
+    {
+        if (_text == null)
+        {
+            return;
+        }
+
+        _text.fontSize = Random.Range(10f, 60f);
+    }
+}
