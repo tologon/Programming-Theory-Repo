274491157ddf8f48718824f3eef3f8a340ea9f50
Tologon Eshimkanov <teshimkanov@gmail.com>
2021-06-08 18:42:46 +0200

    Add ColorButton

---
+++ Assets/Scripts/ColorButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : SimpleButton
+{
+    [SerializeField] private Button _button;
+
+    private Color[] _colors;
+
+    public void Start()
+    {
+        _colors = new Color[] {
+            Color.cyan, Color.grey, Color.gray,
+            Color.magenta, Color.red, Color.yellow,
+            Color.white, Color.green, Color.blue
+        };
+    }
+
+    public override void Action()
+    {
+        if (_button == null)
+        {
+            return;
+        }
+
+        ColorBlock colors = _button.colors;
+        colors.selectedColor = _colors[Random.Range(0, _colors.Length)];
+        _button.colors = colors;
+    }
+}
--- Assets/Scripts/SimpleButton.cs
+++ Assets/Scripts/SimpleButton.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SimpleButton : MonoBehaviour
 {
-    public void Action()
+    public virtual void Action()
     {
         Debug.Log("SimpleButton -> ACTION!");
     }
